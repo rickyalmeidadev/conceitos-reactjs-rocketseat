b1d26729b97e0f2d8e801057100031a2fcb18717
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-08-18 03:11:58 -0300

    feat: add state to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 const App = () => {
+  const [projects, setProjects] = useState([]);
+
+  const handleAddProject = () => {
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+  };
+
   return (
-    <Header title="Hello from React with props!">
+    <>
+      <Header title="Hello from React with props!">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </Header>
+
+
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+        {projects.map(project => (
+          <li key={project}>{project}</li>
+        ))}
       </ul>
-    </Header>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+    </>
   );
 };
 
