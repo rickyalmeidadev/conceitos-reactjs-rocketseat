ddd9dccdca6222dc492bf9f4be6194bac408457c
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-08-18 02:59:50 -0300

    feat: pass children to Header component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import React from 'react';
 import Header from './components/Header';
 
 const App = () => {
-  return <Header title="Hello from React with props!" />;
+  return (
+    <Header title="Hello from React with props!">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </Header>
+  );
 };
 
 export default App;
