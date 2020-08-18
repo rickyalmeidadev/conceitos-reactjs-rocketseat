cdbc2363b878fcfe30ebe8dc284e59ac561a1a3c
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-08-18 02:57:24 -0300

    feat: pass props to Header component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 
 const App = () => {
-  return <Header />;
+  return <Header title="Hello from React with props!" />;
 };
 
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Header = () => {
+const Header = ({ title }) => {
   return (
     <header>
-      <h1>Hello from React!</h1>
+      <h1>{title}</h1>
     </header>
   );
 };
